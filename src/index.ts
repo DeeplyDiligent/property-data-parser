72b0ac090e1d8478f1a8892628f41786c7568a59
Deep Bhattacharyya <dbha0003@student.monash.edu>
2020-08-30 10:04:02 +1000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+const express = require("express");
+const _ = require("lodash");
+
+const app = express();
+const port = 3000;
+
+
+app.get("/", async (req, res) => {
+  
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
