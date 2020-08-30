381e5b632c52fbbb978b5ff4c58fea87a7c0ed93
Deep Bhattacharyya <dbha0003@student.monash.edu>
2020-08-30 10:39:49 +1000

    starter

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,7 @@ const _ = require("lodash");
 const app = express();
 const port = 3000;
 
-
-app.get("/", async (req, res) => {
-  
-});
+app.get("/", async (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
